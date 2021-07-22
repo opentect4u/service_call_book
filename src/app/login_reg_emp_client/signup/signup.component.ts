46c4b423341f3bba081dee082c08835959d5a228
sumanmitra121 <sumanmitra0096@gmail.com>
2021-07-22 08:41:28 +0530

    update sign up validation on 22/7/2021

--- src/app/login_reg_emp_client/signup/signup.component.ts
+++ src/app/login_reg_emp_client/signup/signup.component.ts
@@ -235,14 +235,14 @@ export class SignupComponent implements OnInit {
     this.recaptcha=document.getElementById("capt");
     this.login=true;
     this.Email=document.getElementById("emp_email");
-     this.Name=document.getElementById("emp_name");
+
     console.log("Email:" +this.Email.value);
 
     //  this.f.Email.value=this.Email.value;
-    this.LoginForm.value.name=  this.Name.value;
+    // this.LoginForm.value.name=  this.Name.value;
     this.LoginForm.value.Email=  this.Email.value;
     this.f.Email.setValue(this.LoginForm.value.Email);
-    this.f.name.setValue(this.LoginForm.value.name);
+    // this.f.name.setValue(this.LoginForm.value.name);
     console.log(this.f);
     if(this.LoginForm.invalid){
      console.log("validation");
@@ -354,7 +354,7 @@ export class SignupComponent implements OnInit {
        this.Email.value=this.details[0].email;
        console.log("Email:" +this.Email.value);
        console.log("Email");
-        this.LoginForm.value.name=this.Name.value;
+        // this.LoginForm.value.name=this.Name.value;
         this.LoginForm.value.Email=this.details[0].email;
         this.f.Email.setValue(this.LoginForm.value.Email);
         console.log(this.LoginForm.value);
